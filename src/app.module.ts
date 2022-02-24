debea394e2212f82cda6331f37e228c21a8ea91a
Huii <quanhui812@gmail.com>
2022-02-24 10:23:45 +0700

    update modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,14 @@
+import { join } from 'path'
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { CacheModule } from '@nestjs/common'
+import * as redisStore from 'cache-manager-redis-store'
+import { ServeStaticModule } from '@nestjs/serve-static'
+
 import { AuthModule } from './modules/auth/auth.module'
 import { UsersModule } from './modules/users/users.module'
+import { RolesModule } from './modules/roles/roles.module'
+import { DatabaseModule } from './database/database.module'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
@@ -8,11 +16,27 @@ import { AppService } from './app.service'
 import { APP_GUARD } from '@nestjs/core'
 import { JwtAuthGuard } from './modules/auth/guards/jwt-auth.guard'
 import { RolesGuard } from './core/guards'
-import { CommonModule } from './modules/common/common.module'
-import { RolesModule } from './modules/roles/roles.module'
 
 @Module({
-  imports: [CommonModule, AuthModule, UsersModule, RolesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    CacheModule.register({
+      store: redisStore,
+      url: process.env.REDIS_URL,
+      ttl: 48 * 60 * 60,
+      isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      serveRoot: '/image',
+      rootPath: join(__dirname, '..', 'storage'),
+    }),
+    AuthModule,
+    UsersModule,
+    RolesModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [
     {
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  const app = await NestFactory.create(AppModule)
+  await app.listen(process.env.PORT || 8008)
+  console.log(`Application is running on: ${await app.getUrl()}`)
 }
-bootstrap();
+bootstrap()
